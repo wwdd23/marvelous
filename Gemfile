390cf8d8554e940ef2c65965c38f0e8ccc4c54c8
wwdd23 <wudi@haihuilai.com>
2018-05-10 11:35:21 +0800

    *update) 添加 unicorn

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'jquery-rails'
 gem 'frontend-generators'
+gem 'unicorn'
 
 
 
---
+++ script/restart.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+###################
+# Author:  wudi
+# Mail: programmerwudi@gmail.com 
+# Description: 
+# Created Time: 2018-03-20 18:44:40
+###################
+
+echo "look pid"
+ps aux |grep unicorn
+
+echo "git pull start..."
+git pull; 
+
+echo "assets"
+rails assets:precompile
+
+echo "bundle installing"
+bundle install; kill -9 `cat /opt/work/marvelous/tmp/pids/marvelous_peach.pid` ;  bundle exec unicorn_rails -E production -D -c /opt/work/marvelous/config/unicorn.rb; ps aux |grep unicorn
+
+
+#god stop simple
+#echo "restart god"
+#god -c simple.god 
+
+
+echo "Restart end.."
